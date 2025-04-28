7476c73917b6684fdfa42092ba96719c8f82a835
Josh Basquez <69749376+joshbasquez@users.noreply.github.com>
2025-04-28 08:38:38 -0500

    Create connect-via-appRegistration.ps1

---
+++ authentication/connect-via-appRegistration.ps1
@@ -0,0 +1,27 @@
+# Retrieve Token
+$tenantId = "xxx"
+$clientId = "xxx"
+$clientSecret = "xxx"
+$tokenUrl = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+# Create request body
+$body = @{
+    client_id     = $clientId
+    client_secret = $clientSecret
+    scope         = "https://api.securitycenter.microsoft.com/.default"
+    grant_type    = "client_credentials"
+}
+$response = Invoke-RestMethod -Uri $tokenUrl -Method Post -Body $body -ContentType "application/x-www-form-urlencoded"
+$accessToken = $response.access_token
+
+$headers = @{
+    Authorization = "Bearer $accessToken"
+    "Content-Type" = "application/json"
+}
+"API Connected."
+
+# get devices
+$devices = Invoke-RestMethod -Uri "https://api.security.microsoft.com/api/machines" -Headers $headers
+"devices collected: "
+$devices.value | measure
+
+$devices.value
