e7670703763d132f1ff853e72f5802117adb1423
Josh Basquez <69749376+joshbasquez@users.noreply.github.com>
2025-04-28 08:56:10 -0500

    Create SecurityAPI-Certificate-Authentication.ps1

---
+++ authentication/SecurityAPI-Certificate-Authentication.ps1
@@ -0,0 +1,39 @@
+# Connect to the Defender SecurityAPI using certificate authentication
+
+# pre-requisites:
+<#
+ - install the module MSAL.PS
+ - request a certificate from company CA or generate self-signed private key
+ - upload private key to the app registration in azure
+ - load private key to the local key store of client connecting to securityAPI
+ - app registration (clientID) must have API permissions for defender.
+     example reading machine information will need machine.read.all
+     permissions added in entra > applications > app registrations > "app name" 
+       > API permissions > Add a permission > search "WindowsDefenderATP" > Add machine.read.all or machine.readwrite.all
+--
+ Cert path info:
+ - example cert is loaded into CurrentUser\Personal
+ - get cert thumbprint using:
+    Get-ChildItem -path 'cert:\currentuser\my' | fl friendlyname, subject, thumbprint
+ - powershell path to currentuser\personal is 
+    get-item -Path 'Cert:\CurrentUser\my\xxthumbprintxxx'
+#>
+
+Import-Module MSAL.PS
+
+$connectionDetails = @{
+    'TenantId'          = '4b11xx-xxx-xxx'
+    'ClientId'          = '1ddxxx-xxx-xxx'
+    'ClientCertificate' = Get-Item -Path 'Cert:\CurrentUser\my\xxthumbprintxx' 
+}
+
+# Acquire a token
+$token = (Get-MsalToken @connectionDetails -Scopes 'https://api.securitycenter.microsoft.com/.default').accessToken
+
+$headers = @{
+    "Authorization" = "Bearer $Token"
+}
+
+# test query of machines
+$devices = Invoke-RestMethod -Uri "https://api.security.microsoft.com/api/machines" -Headers $headers
+$devices.value
